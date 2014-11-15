8d6b4fa88653cf66cfe84dd9cdccd96ed2f2dae7
Joshua Tanner <biolsurfer85@gmail.com>
2014-11-15 15:09:12 -0800

    Add player.js

---
+++ js/player.js
@@ -0,0 +1,25 @@
+var player = (function () {
+	var playerName, playerScore;
+
+	var _increaseScore = function (score) {
+		// increase score
+	};
+
+	var checkAnswer = function (point) {
+		// check to see if answer is correct
+	};
+
+	var setPlayerName = function (name) {
+		// set player name
+	};
+
+	var getPlayerName = function () {
+		// get player name
+	};
+
+	return {
+		setPlayerName : setPlayerName,
+		getPlayerName : getPlayerName,
+		checkAnswer : checkAnswer
+	}
+}());
